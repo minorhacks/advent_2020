be4e6f8e44fea9c0a5b3dab05bffd4ff0cbf7e38
Scott Minor <minor@minorhacks.com>
2020-12-13 02:06:49 -0800

    day_13: Passed part 2

--- src/bin/day_13/main.rs
+++ src/bin/day_13/main.rs
@@ -3,7 +3,7 @@ use advent_2020::shuttle;
 fn main() {
     let input = std::fs::read_to_string("src/bin/day_13/input.txt").unwrap();
     let input = input.trim();
-    let now = input.lines().nth(0).unwrap().parse::<i32>().unwrap();
+    let now = input.lines().nth(0).unwrap().parse::<u64>().unwrap();
     let shuttles = input
         .lines()
         .nth(1)
@@ -15,4 +15,6 @@ fn main() {
         "Part 1: {}",
         next_id * shuttle::minutes_to_wait(now, next_id)
     );
+
+    println!("Part 2: {}", shuttles.leaving_consecutively());
 }
--- src/shuttle.rs
+++ src/shuttle.rs
@@ -8,7 +8,7 @@ pub enum Error {
 
 enum Shuttle {
     OutOfService,
-    Id(i32),
+    Id(u64),
 }
 
 type Result<T> = std::result::Result<T, Error>;
@@ -24,7 +24,7 @@ impl std::str::FromStr for Shuttle {
         } else {
             Ok(Shuttle::Id(
                 s.trim()
-                    .parse::<i32>()
+                    .parse::<u64>()
                     .map_err(|_| Error::ShuttleParseError)?,
             ))
         }
@@ -44,12 +44,12 @@ impl std::str::FromStr for ShuttleList {
     }
 }
 
-pub fn minutes_to_wait(now: i32, id: i32) -> i32 {
+pub fn minutes_to_wait(now: u64, id: u64) -> u64 {
     id - (now.rem_euclid(id))
 }
 
 impl ShuttleList {
-    pub fn next_shuttle(&self, now: i32) -> i32 {
+    pub fn next_shuttle(&self, now: u64) -> u64 {
         *self
             .0
             .iter()
@@ -59,6 +59,28 @@ impl ShuttleList {
             })
             .min_by_key(|&&id| minutes_to_wait(now, id))
             .unwrap()
+    }
+
+    pub fn leaving_consecutively(&self) -> u64 {
+        let mut shuttle_iter = self
+            .0
+            .iter()
+            .enumerate()
+            .filter_map(|(i, shuttle)| match shuttle {
+                Shuttle::Id(id) => Some((i, id)),
+                _ => None,
+            });
+        let (time, step) = shuttle_iter.nth(0).unwrap();
+        let mut time = time as u64;
+        let mut step = step.clone();
+        for (offset, &id) in shuttle_iter {
+            let offset = offset as u64;
+            while (time + offset) % id != 0 {
+                time = time + step;
+            }
+            step = step * id;
+        }
+        time
     }
 }
 
@@ -72,5 +94,51 @@ mod tests {
     fn test_next_shuttle() {
         let shuttles = TEST_INPUT.parse::<ShuttleList>().unwrap();
         assert_eq!(59, shuttles.next_shuttle(939));
+    }
+
+    #[test]
+    fn test_leaving_consecutively() {
+        assert_eq!(
+            1068781,
+            TEST_INPUT
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
+        assert_eq!(
+            3417,
+            "17,x,13,19"
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
+        assert_eq!(
+            754018,
+            "67,7,59,61"
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
+        assert_eq!(
+            779210,
+            "67,x,7,59,61"
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
+        assert_eq!(
+            1261476,
+            "67,7,x,59,61"
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
+        assert_eq!(
+            1202161486,
+            "1789,37,47,1889"
+                .parse::<ShuttleList>()
+                .unwrap()
+                .leaving_consecutively()
+        );
     }
 }
