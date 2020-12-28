8ddae3accdbd2db802a73d701f9393d4399e6282
Scott Minor <minor@minorhacks.com>
2020-12-27 16:33:14 -0800

    day_23: Passed part 2

--- src/bin/day_23/main.rs
+++ src/bin/day_23/main.rs
@@ -2,7 +2,23 @@ use advent_2020::cups;
 
 fn main() {
     let input = std::fs::read_to_string("src/bin/day_23/input.txt").unwrap();
-    let mut cups = input.trim().parse::<cups::Cups>().unwrap();
+    let cup_list = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<_>>();
+    let mut cups = cups::Cups::new(&cup_list);
     cups.run(100);
     println!("Part 1: {}", cups.order_after(1));
+
+    let cup_list = cup_list
+        .iter()
+        .cloned()
+        .chain(cup_list.len() as u32 + 1..=1000000)
+        .collect::<Vec<_>>();
+    let mut cups = cups::Cups::new(&cup_list);
+    cups.run(10000000);
+    println!(
+        "Part 2: {}",
+        cups.first_n_after(1, 2).iter().product::<u64>()
+    );
 }
--- src/cups.rs
+++ src/cups.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::collections::HashMap;
 use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
@@ -7,68 +7,54 @@ pub enum Error {
     CupParse(char),
 }
 
-type Result<T> = std::result::Result<T, Error>;
+//type Result<T> = std::result::Result<T, Error>;
 
 pub struct Cups {
-    c: VecDeque<u32>,
+    next_map: HashMap<u32, u32>,
     max: u32,
-    front: u32,
-}
-
-impl std::str::FromStr for Cups {
-    type Err = Error;
-
-    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
-        let c = s
-            .trim()
-            .chars()
-            .map(|c| c.to_digit(10).ok_or(Error::CupParse(c)))
-            .collect::<Result<VecDeque<u32>>>()?;
-        let &max = c.iter().max().unwrap();
-        Ok(Cups { c, max, front: 0 })
-    }
+    current: u32,
 }
 
 impl Cups {
-    pub fn step(&mut self) {
-        let current = *self.c.iter().next().unwrap();
-        let temp = self.c.pop_front().unwrap();
-        self.c.push_back(temp);
-        // Pick up 3 cups after current cupub p
-        let mut picked_up = VecDeque::new();
-        for _ in 0..3 {
-            picked_up.push_back(self.c.pop_front().unwrap())
+    pub fn new(cup_list: &[u32]) -> Cups {
+        let first = *cup_list.iter().next().unwrap();
+        let mut i = cup_list.iter().peekable();
+        let mut next_map = HashMap::new();
+        while let Some(v) = i.next() {
+            next_map.insert(*v, **i.peek().unwrap_or(&&first));
         }
+        let &max = cup_list.iter().max().unwrap();
+        Cups {
+            next_map,
+            max,
+            current: first,
+        }
+    }
+
+    pub fn step(&mut self) {
+        // Take current cup
+        let taken_1 = *self.next_map.get(&self.current).unwrap();
+        let taken_2 = *self.next_map.get(&taken_1).unwrap();
+        let taken_3 = *self.next_map.get(&taken_2).unwrap();
+
         // Calculate new cup, which is src - 1, or keep decrementing mod #cups
         // until we find a cup that isn't picked up
-        let mut dest = ((current as i32 - 2).rem_euclid(self.max as i32) + 1) as u32;
-        while picked_up.contains(&dest) {
+        let mut dest = ((self.current as i32 - 2).rem_euclid(self.max as i32) + 1) as u32;
+        while [taken_1, taken_2, taken_3].contains(&dest) {
             dest = ((dest as i32 - 2).rem_euclid(self.max as i32) + 1) as u32;
         }
-        // new array:
-        let mut new_cups = VecDeque::new();
-        // * current cup
-        new_cups.push_back(self.c.pop_back().unwrap());
-        // * everything between current cup up to and including dest cup, except
-        //   for cups that are picked up
-        let mut temp = None;
-        while temp != Some(dest) {
-            temp = Some(self.c.pop_front().unwrap());
-            new_cups.push_back(temp.unwrap());
-        }
-        // * cups that were picked up
-        while !picked_up.is_empty() {
-            new_cups.push_back(picked_up.pop_front().unwrap());
-        }
-        // * any remaining cups
-        while !self.c.is_empty() {
-            new_cups.push_back(self.c.pop_front().unwrap());
-        }
-        self.c = new_cups;
-        // Shuffle around until next current is in front
-        let temp = self.c.pop_front().unwrap();
-        self.c.push_back(temp);
-        self.front = (self.front + 1).rem_euclid(self.max);
+
+        // current's next is taken_3's old next
+        self.next_map
+            .insert(self.current, *self.next_map.get(&taken_3).unwrap());
+        // taken_3's next is dest's old next
+        self.next_map
+            .insert(taken_3, *self.next_map.get(&dest).unwrap());
+        // dest's next is taken_1
+        self.next_map.insert(dest, taken_1);
+
+        // Update current
+        self.current = *self.next_map.get(&self.current).unwrap();
     }
 
     pub fn run(&mut self, num_steps: usize) {
@@ -77,36 +63,22 @@ impl Cups {
         }
     }
 
-    #[cfg(test)]
-    fn order(&mut self) -> String {
-        for _ in 0..self.front {
-            let temp = self.c.pop_back().unwrap();
-            self.c.push_front(temp);
-        }
-        let s = self
-            .c
-            .iter()
-            .map(|i| i.to_string())
-            .collect::<Vec<_>>()
-            .join("");
-        for _ in 0..self.front {
-            let temp = self.c.pop_front().unwrap();
-            self.c.push_back(temp);
-        }
-        s
-    }
-
     pub fn order_after(&self, c: u32) -> String {
-        let mut copy = self.c.clone();
-        while c != *copy.front().unwrap() {
-            let temp = copy.pop_front().unwrap();
-            copy.push_back(temp);
-        }
-        copy.into_iter()
-            .skip(1)
+        let nums = self.first_n_after(c, self.next_map.len() - 1);
+        nums.iter()
             .map(|i| i.to_string())
             .collect::<Vec<_>>()
             .join("")
+    }
+
+    pub fn first_n_after(&self, c: u32, n: usize) -> Vec<u64> {
+        let mut first_n = Vec::new();
+        let mut travel = c;
+        for _i in 0..n {
+            travel = *self.next_map.get(&travel).unwrap();
+            first_n.push(travel as u64);
+        }
+        first_n
     }
 }
 
@@ -114,37 +86,54 @@ impl Cups {
 mod tests {
     use super::*;
 
+    static TEST_ORDER: &[u32] = &[3, 8, 9, 1, 2, 5, 4, 6, 7];
+
     #[test]
     fn test_step_order() {
-        let mut cups = "389125467".parse::<Cups>().unwrap();
+        let mut cups = Cups::new(TEST_ORDER);
         cups.step();
-        assert_eq!("328915467", cups.order());
+        assert_eq!("28915467", cups.order_after(3));
         cups.step();
-        assert_eq!("325467891", cups.order());
+        assert_eq!("25467891", cups.order_after(3));
         cups.step();
-        assert_eq!("725891346", cups.order());
+        assert_eq!("25891346", cups.order_after(7));
         cups.step();
-        assert_eq!("325846791", cups.order());
+        assert_eq!("25846791", cups.order_after(3));
         cups.step();
-        assert_eq!("925841367", cups.order());
+        assert_eq!("25841367", cups.order_after(9));
         cups.step();
-        assert_eq!("725841936", cups.order());
+        assert_eq!("25841936", cups.order_after(7));
         cups.step();
-        assert_eq!("836741925", cups.order());
+        assert_eq!("36741925", cups.order_after(8));
         cups.step();
-        assert_eq!("741583926", cups.order());
+        assert_eq!("41583926", cups.order_after(7));
         cups.step();
-        assert_eq!("574183926", cups.order());
+        assert_eq!("74183926", cups.order_after(5));
         cups.step();
-        assert_eq!("583741926", cups.order());
+        assert_eq!("83741926", cups.order_after(5));
     }
 
     #[test]
     fn test_run_order_after() {
-        let mut cups = "389125467".parse::<Cups>().unwrap();
+        let mut cups = Cups::new(TEST_ORDER);
         cups.run(10);
         assert_eq!("92658374", cups.order_after(1));
         cups.run(90);
         assert_eq!("67384529", cups.order_after(1));
+    }
+
+    #[test]
+    #[cfg_attr(not(feature = "expensive_tests"), ignore)]
+    fn test_million_cups() {
+        let all_cups = TEST_ORDER
+            .iter()
+            .cloned()
+            .chain(TEST_ORDER.len() as u32 + 1..=1000000)
+            .collect::<Vec<_>>();
+        let mut cups = Cups::new(&all_cups);
+        cups.run(10000000);
+        let two_front = cups.first_n_after(1, 2);
+        assert_eq!(934001, two_front[0]);
+        assert_eq!(159792, two_front[1]);
     }
 }
